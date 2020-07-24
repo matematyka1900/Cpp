156745e90a57585bbe3a4d4a9c93f0e4e05526ff
Maciej Pokora <maciejpokora111@gmail.com>
2020-07-24 22:11:57 +0200

    NWD

---
+++ NWD.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	float a, b, c;
+	float delta;
+	int n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		
+
+		delta = ((b*b) - 4 * a * c);
+		if (delta > 0)
+		{
+			cout << 2 << endl;
+		}
+		else if (delta == 0)
+		{
+			cout << 1 << endl;
+
+		}
+		else
+		{
+			cout << 0 << endl;
+		}
+
+		
+	}
+	
+}
